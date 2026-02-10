25d25cfa9786d5a3391b31598292571aa5005844
Vladenceya <vladenceya_vladenceya111@mail.ru>
2026-02-10 17:48:34 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Появление элементов при скролле
 const faders = document.querySelectorAll('.fade-in');
 
 const appearOptions = {
@@ -17,3 +16,19 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer){
 }, appearOptions);
 
 faders.forEach(fader => appearOnScroll.observe(fader));
+
+const langBtn = document.getElementById('switch-lang');
+let lang = 'ru';
+
+langBtn.addEventListener('click', () => {
+  const allText = document.querySelectorAll('[data-ru]');
+  if(lang === 'ru'){
+    allText.forEach(el => el.textContent = el.getAttribute('data-en'));
+    langBtn.textContent = 'RU';
+    lang = 'en';
+  } else {
+    allText.forEach(el => el.textContent = el.getAttribute('data-ru'));
+    langBtn.textContent = 'EN';
+    lang = 'ru';
+  }
+});
